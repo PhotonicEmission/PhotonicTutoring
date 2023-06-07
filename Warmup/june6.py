f969049d5b64fe56e155c866c48626a5799e3ec6
William Coolman <wcoolfox@gmail.com>
2023-06-06 21:59:59 -0700

    Commented out exercise Changes made to make debugging exercise easier

--- Warmup/june6.py
+++ Warmup/june6.py
@@ -10,11 +10,16 @@ not 6 == 7
 # int(105)/int(4)
 int(105/4)
 #intialize x=7, iterate over: increment by 3 everytime, until it is at least 20.
-x=7
-while x<20:
-    x+=3
-print(x)
+# x=7
+# while x<20:
+#     x+=3
+# print(x)
 #take user input, and add 20 to it, print the result    
-x=input("Enter an integer")
-x=20+int(x)
-print(x)
+# x=input("Enter an integer")
+# x=20+int(x)
+# print(x)
+spam=[1,2,3]
+for x in spam*10:
+    print(x)
+
+# This program is a loop that takes the variable spam, and prints it line by line as it loops through the loop 10 times. In this case, because spam specifically has a list in it, the output is printed one line at a time rather than on a single line.
