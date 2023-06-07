5666417adbf97565e6ce0276cada36c4cf01500a
William Coolman <wcoolfox@gmail.com>
2023-06-06 21:10:53 -0700

    June 6th warmup reviewed booleans, conditionals, and basic things.
    Co-authored-by: VKhan <VladFurman@users.noreply.github.com>

---
+++ Warmup/june6.py
@@ -0,0 +1,20 @@
+#use not in a statement
+not 6 == 7
+#use or in a statement. CORRECTLY.
+(True or False) == True
+(True and False) == False
+# A*2 = A+A
+# 4*2 = 4+4
+# "apple"*2 = "appleapple"
+34%100
+# int(105)/int(4)
+int(105/4)
+#intialize x=7, iterate over: increment by 3 everytime, until it is at least 20.
+x=7
+while x<20:
+    x+=3
+print(x)
+#take user input, and add 20 to it, print the result    
+x=input("Enter an integer")
+x=20+int(x)
+print(x)
