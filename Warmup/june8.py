30d9f691c263f81d92afa053db85c28da64dac32
PhotonicEmission <wcoolfox@gmail.com>
2023-06-11 21:02:12 -0700

    MADE EXERCISE, DID EXERCISE

--- Warmup/june8.py
+++ Warmup/june8.py
@@ -64,10 +64,17 @@
 # print(round(my_temp))
 
 #read integers from the user until it is not an integer, and then print all the integers back.
+userList= []
+# userItem=(int(input("Input an integer: ")))
 
-userInt=input("Input an integer: ")
 try:
     while True:
-        print(userInt)
+    #while isinstance(userItem,int)==True:
+        #userList.append(userItem)
+        
+        userList.append(int(input("Input an integer: ")))
+        continue
 except:
     print('That is not an integer.')
+print('\n'+"Here are the integers you enetered: ")    
+print(*userList,sep='\n')
